57a181e3d948135460aa42af77ad1ffda8422ef1
YulHeo <sieazz@outlook.com>
2022-05-20 16:12:43 +0900

    Apply sqlite3

--- stock_hit/stockHit.py
+++ stock_hit/stockHit.py
@@ -1,7 +1,7 @@
 import collections
 import datetime
-import json
 import schedule
+import sqlite3
 import telegram
 import time
 import yfinance as yf
@@ -9,133 +9,143 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from threading import Thread
 
 
-reqlist = {}
+db = sqlite3.connect("reqlist.db", check_same_thread=False)
+cursor = db.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS request_list(key INTEGER, symbol TEXT, user INTEGER, target FLOAT, isLower INTEGER)")
+
+
+ # get telegram bot token
+with open('token', 'r') as f:
+    token = f.readline()[:-1]
+
+updater = Updater(token, use_context=True)
+bot = updater.bot
+
+
+# db의 index를 설정
+cursor.execute("SELECT MAX(key) FROM request_list")
+tmp = cursor.fetchone()[0]
+next_key = tmp+1 if tmp else 1
 
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="To set the stock price alarm,\n"
-                                  "enter {stock symbol} {stock price}")
+                            text="To set the stock price alarm,\n"
+                                "enter {stock symbol} {stock price}")
 
 
 def set_alarm(update, context):
-    global reqlist
-
+    global next_key
     args = update.message.text.split(" ")
 
     if len(args) == 2:
         try:
-            stock_symbol = args[0].upper()
-            ticker = yf.Ticker(stock_symbol)
-            req = {}
-            req['user'] = update.message.from_user.id
-            req['target_price'] = float(args[1])
-            if req['target_price'] < ticker.history(period='1d')['Close'][0]:
-                req['target_is_lower'] = True
+            symbol = args[0].upper()
+            ticker = yf.Ticker(symbol)
+            user = update.message.from_user.id
+            target = float(args[1])
+            if target < ticker.history(period='1d')['Close'][0]:
+                isLower = 1
             else:
-                req['target_is_lower'] = False
-            reqlist[stock_symbol].append(req)
-
+                isLower = 0
 
         except IndexError:
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"Stock symbol {stock_symbol} is not available")
+                                    text=f"Stock symbol {symbol} is not available")
         except ValueError:
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text="Stock price should be a number")
+                                    text="Stock price should be a number")
         else:
-            print(f"add alarm\n{reqlist}")
+            print(f"add alarm\nINSERT INTO request_list VALUES({symbol}, {user}, {target}, {isLower})")
+            cursor.execute(f"INSERT INTO request_list VALUES({next_key}, '{symbol}', {user}, {target}, {isLower})")
+            next_key += 1
+            db.commit()
+            
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"Your request ({stock_symbol}, ${req['target_price']}) was successfully submitted!")
+                                    text=f"Your request ({symbol}, ${target}) was successfully submitted!")
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"The alarm will be sent to you if {stock_symbol}(${str(round(ticker.history(period='1d')['Close'][0], 2))}) hits that price..")
+                                    text=f"The alarm will be sent to you if {symbol}(${str(round(ticker.history(period='1d')['Close'][0], 2))}) hits that price..")
     else:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Input form should be\n'{stock symbol} {stock price}'\n"
-                                      "Example: MSFT 350")
+                                text="Input form should be\n'{stock symbol} {stock price}'\n"
+                                    "Example: MSFT 350")
 
 
 def show_alarm(update, context):
-    pass
-
+    cursor.execute(f"SELECT symbol, target FROM request_list WHERE user = {update.effective_chat.id}")
+    reply = ["  ".join((req[0], str(req[1]), str(round(yf.Ticker(req[0]).history(period='1d')['Close'][0], 3)), '\n'))
+                for req in cursor.fetchall()]
+    if reply:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="".join(reply))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Your request not exists.")
+        
 
 def del_alarm(update, context):
     pass
 
 
 def real_time_work(bot):
-    global reqlist
-
-    def sync_list():
-        empty_list = []
-        for stock_symbol in reqlist:
-            if not reqlist[stock_symbol]:
-                empty_list.append(stock_symbol)
-        print(f"sync jsonfile\n{reqlist}")
-        for stock_symbol in empty_list:
-            del reqlist[stock_symbol]
-        with open("reqlist.json", "w") as f:
-            json.dump(reqlist, f, indent=4)
-
-
+    
+    # 주가 확인 후 사용자에게 메세지 보내고 db에서 삭제
     def alarm(bot):
         current_time = datetime.datetime.now()
         # if current_time.hour < 14 or current_time >= 21:
         #     return
-        for stock_symbol, reqs in reqlist.items():
-            ticker = yf.Ticker(stock_symbol)
+
+        cursor.execute("SELECT DISTINCT(symbol) FROM request_list")
+        symbols = cursor.fetchall()
+
+        delete_key_list = []
+        for symbol in symbols:
+            symbol = symbol[0]
+            ticker = yf.Ticker(symbol)
             current_price = ticker.history(period='1d')['Close'][0]
-            reqs_done = []
+            
+            cursor.execute(f"SELECT key, user, target, isLower FROM request_list WHERE symbol='{symbol}'")
+            reqs = cursor.fetchall()
             try:
                 for req in reqs:
-                    if req['target_is_lower'] and req['target_price'] >= current_price:
-                        bot.sendMessage(chat_id=req['user'],
+                    key, user, target, isLower = req[0], req[1], req[2], req[3]
+                    if isLower and target >= current_price:
+                        bot.sendMessage(chat_id=user,
                                         text=f"{current_time.strftime('%Y/%m/%d %H:%M:%S')} (UTC)\n"
-                                             f"{stock_symbol}(${str(round(current_price, 2))}) hit the target price ${req['target_price']}! \n"
+                                             f"{symbol}(${str(round(current_price, 2))}) hit the target price ${target}! \n"
                                              f"target_is_lower chance?")
-                        reqs_done.append(req)
-                    elif (not req['target_is_lower']) and req['target_price'] <= current_price:
-                        bot.sendMessage(chat_id=req['user'],
+                        delete_key_list.append(key)
+                    elif (not isLower) and target <= current_price:
+                        bot.sendMessage(chat_id=user,
                                         text=f"{current_time.strftime('%Y/%m/%d %H:%M:%S')} (UTC)\n"
-                                             f"{stock_symbol}(${str(round(current_price, 2))}) hit the target price ${req['target_price']}! \n"
+                                             f"{symbol}(${str(round(current_price, 2))}) hit the target price ${target}! \n"
                                              f"Time to sell?")
-                        reqs_done.append(req)
+                        delete_key_list.append(key)
             except telegram.error.Unauthorized:
                 pass
-            for req_done in reqs_done:
-                reqs.remove(req_done)
+            
+            for key in delete_key_list:
+                cursor.execute(f"DELETE FROM request_list WHERE key={key}")
+        
+        db.commit()
 
-    schedule.every().hour.do(sync_list)
-    schedule.every(1).minutes.do(alarm, bot)
+
+    schedule.every().minutes.do(alarm, bot)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
-if __name__ == "__main__":
-    with open('reqlist.json', 'r', encoding='UTF-8') as f:
-        reqlist = collections.defaultdict(list, json.load(f))
+dispatcher = updater.dispatcher
 
-    # get telegram bot token
-    with open('token', 'r') as f:
-        token = f.readline()[:-1]
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('show', show_alarm))
+dispatcher.add_handler(CommandHandler('del', del_alarm))
+dispatcher.add_handler(MessageHandler(Filters.text, set_alarm))
 
-    updater = Updater(token, use_context=True)
 
-    bot = updater.bot
+thread = Thread(target=real_time_work, args=(bot,))
+thread.start()
 
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('show', show_alarm))
-    dp.add_handler(CommandHandler('del', del_alarm))
-    dp.add_handler(MessageHandler(Filters.text, set_alarm))
-
-    thread = Thread(target=real_time_work, args=(bot,))
-    thread.start()
-    
-    updater.start_polling()
-    updater.idle()
-
+updater.start_polling()
+updater.idle()
